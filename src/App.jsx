ded502b55e7d477ef6e3a998cd3b320de50f7a88
BLOCKMATERIAL <miroslavgrischenko@gmail.com>
2021-06-10 16:18:04 +0300

    Added html template and fetch request for get random quote.

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,40 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React, { useState,useEffect } from 'react'
 import './App.css'
+import {useDispatch, useSelector} from "react-redux";
+import {setData} from "./store/store";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = useSelector(state => state.data)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const loadData = async () => {
+      const postId = Math.floor(Math.random() * 100)
+      const dataRaw = await fetch('https://jsonplaceholder.typicode.com/posts/' + postId)
+      if (!dataRaw.ok) {
+        dispatch(setData('Ошибка загрузки'))
+        return
+      }
+      const dataJson = await dataRaw.json()
+      dispatch(setData(dataJson.body))
+    }
+
+    loadData()
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        {!data &&
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
+          ...подождите...
         </p>
+        }
+        {data &&
         <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
+          {data}
         </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        }
       </header>
     </div>
   )
